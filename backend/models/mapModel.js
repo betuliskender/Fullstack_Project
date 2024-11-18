47fea9aaf4dc483ede47f9e063b5e9b4f98ba7a9
Yelong Hartl-He <Yelong.he@hotmail.com>
2024-11-18 10:49:56 +0100

    added pin locations to mapModel

--- backend/models/mapModel.js
+++ backend/models/mapModel.js
@@ -1,27 +1,32 @@
 import mongoose from "mongoose";
 
-const mapSchema = new mongoose.Schema(
-  {
-    pinLocation: {
-      type: String,
-      required: false,
-    },
-    imageURL: {
-      type: String,
-      required: true,
-    },
-    campaign: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Campaign",
-      required: true,
-    },
-    session: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Session",
-      required: false,
-    },
+const mapSchema = new mongoose.Schema({
+  pinLocation: {
+    type: String,
+    required: false,
   },
-);
+  imageURL: {
+    type: String,
+    required: true,
+  },
+  campaign: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Campaign",
+    required: true,
+  },
+  session: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Session",
+    required: false,
+  },
+  pins: [
+    {
+      x: { type: Number, required: true },
+      y: { type: Number, required: true },
+      character: { type: mongoose.Schema.Types.ObjectId, ref: "Character" },
+    },
+  ],
+});
 
 const Map = mongoose.model("Map", mapSchema);
 export default Map;
