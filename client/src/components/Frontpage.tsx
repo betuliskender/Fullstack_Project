3d53332395bd08d7c90cbaaa10ca491300314e8c
Yelong Hartl-He <Yelong.he@hotmail.com>
2024-12-08 21:13:21 +0100

    added clouds in light mode and removed d20 dice temp

--- client/src/components/Frontpage.tsx
+++ client/src/components/Frontpage.tsx
@@ -1,20 +1,32 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stars } from "@react-three/drei";
-import { Box } from "@chakra-ui/react";
-import D20 from "./D20";
+import { OrbitControls, Stars, Cloud } from "@react-three/drei";
+import { Box, useColorMode } from "@chakra-ui/react";
+//import D20 from "./D20";
 
 const FrontPage: React.FC = () => {
+  const { colorMode } = useColorMode();
+
+  const textColor = colorMode === "dark" ? "white" : "#2D3748";
+
   return (
     <Box position="relative" h="100vh" w="100vw" overflow="hidden">
       {/* Three.js Canvas */}
       <Canvas
         camera={{ position: [0, 2, 5] }}
-        style={{ position: "absolute", top: 0, left: 0 }}
+        style={{ position: "absolute", top: 0, left: 0 , height: "100vh", width: "100vw" }}
       >
-        <Stars />
+        {colorMode === "dark" ? (
+          <Stars fade saturation={1} />
+        ) : (
+          <>
+            <Cloud position={[0, 2, -5]} speed={0.2} opacity={0.5} />
+            <Cloud position={[-5, 1, -5]} speed={0.3} opacity={0.4} />
+            <Cloud position={[5, 1, -5]} speed={0.25} opacity={0.4} />
+          </>
+        )}
         <ambientLight intensity={0.3} />
         <directionalLight position={[5, 5, 5]} intensity={1} />
-        <D20 />
+        {/* <D20 /> */}
         <OrbitControls enableZoom={false} />
       </Canvas>
 
@@ -24,7 +36,7 @@ const FrontPage: React.FC = () => {
         top="50%"
         left="50%"
         transform="translate(-50%, -50%)"
-        color="white"
+        color={textColor}
         textAlign="center"
         zIndex="10"
         pointerEvents="none"
@@ -47,16 +59,6 @@ const FrontPage: React.FC = () => {
           Your adventure starts here...
         </p>
       </Box>
-
-      {/* Buttons */}
-      <Box
-        position="absolute"
-        top="20px"
-        right="20px"
-        zIndex="11"
-        display="flex"
-        gap="10px"
-      ></Box>
     </Box>
   );
 };
