81646bfae6e12a56b2e5530ec2ab8feda12d8da4
Yelong Hartl-He <Yelong.he@hotmail.com>
2024-12-10 13:20:06 +0100

    changed cors settings

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://fullstack-project-pn67qpivc-betuliskenders-projects.vercel.app/", "http://localhost:5173"],
+  credentials: true,
+}));
 app.use(express.json());
 
 connectDB(); 
