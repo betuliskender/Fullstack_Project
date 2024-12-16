0423165529ad94fd44821fb40a312ef88b649d79
Yelong Hartl-He <Yelong.he@hotmail.com>
2024-12-16 17:43:26 +0100

    Added sentry.io to frontend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/fullstack-project-psi\.vercel\.app/, /^https:\/\/fullstack-project-1-yz4i\.onrender\.com/],debug:true,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
